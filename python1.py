5cdc5b1136ecd0d76397dc090804801abc07a0f7
ZhanaiymDauken0216 <114014269+ZhanaiymDauken0216@users.noreply.github.com>
2022-09-21 11:39:02 +0600

    Update python1.py

--- python1.py
+++ python1.py
@@ -1,7 +1,11 @@
 n = int(input("Введите число:"))
-tot = 0
+suma=0
+mult = 1
 while(n > 0):
     dig = n % 10
-    tot = tot - dig
+    if dig !=0:
+        suma+=dig
+        mult*=dig
     n = n//10
-print("Сумма цифр равна:", tot)
+print("Сумма цифр равна:", suma)
+print("произведение:",mult)
